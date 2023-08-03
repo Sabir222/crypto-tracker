99d3e17514e7325072d9297c9592f85315b905ab
Sabir222 <mrsabir4@gmail.com>
2023-08-03 01:28:19 +0100

    First commit

--- context/store.ts
+++ context/store.ts
@@ -1,40 +1,40 @@
-import { create } from "zustand";
+// import { create } from "zustand";
 
-const apiEndpoint =
-  "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
-const apiKey = process.env.CMC_PRO_API_KEY;
-const headers: HeadersInit = apiKey
-  ? {
-      "X-CMC_PRO_API_KEY": apiKey,
-    }
-  : {};
+// const apiEndpoint =
+//   "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+// const apiKey = process.env.CMC_PRO_API_KEY;
+// const headers: HeadersInit = apiKey
+//   ? {
+//       "X-CMC_PRO_API_KEY": apiKey,
+//     }
+//   : {};
 
-interface CoinDataProps {
-  id: number;
-  name: string;
-}
-interface CoinStore {
-  coins: CoinDataProps[];
-  fetchCoins: () => Promise<void>;
-}
+// interface CoinDataProps {
+//   id: number;
+//   name: string;
+// }
+// interface CoinStore {
+//   coins: CoinDataProps[];
+//   fetchCoins: () => Promise<void>;
+// }
 
-const useCoinStore = create<CoinStore>((set) => ({
-  coins: [],
-  fetchCoins: async () => {
-    try {
-      const response = await fetch(apiEndpoint, {
-        headers: headers,
-      });
-      if (!response.ok) {
-        throw new Error("Failed to fetch Data");
-      }
-      const data = await response.json();
-      const coins = data.data;
-      set({ coins });
-    } catch (error) {
-      console.error(error);
-    }
-  },
-}));
+// const useCoinStore = create<CoinStore>((set) => ({
+//   coins: [],
+//   fetchCoins: async () => {
+//     try {
+//       const response = await fetch(apiEndpoint, {
+//         headers: headers,
+//       });
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch Data");
+//       }
+//       const data = await response.json();
+//       const coins = data.data;
+//       set({ coins });
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   },
+// }));
 
-export default useCoinStore;
+// export default useCoinStore;
