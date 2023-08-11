4f37071beaf50b98294a575362edeb59df52ee34
Sabir222 <mrsabir4@gmail.com>
2023-08-11 16:02:15 +0100

    Fuck you vercel

--- components/Model.tsx
+++ components/Model.tsx
@@ -72,68 +72,64 @@ const Model: React.FC<ModelProps> = ({ toggleVisible }) => {
   console.log(value.toUpperCase());
 
   return (
-    <>
-      <main className="absolute top-0 bottom-0 left-0 right-0 bg-black/40 ">
-        <div className="flex items-center justify-center h-[100vh] ">
-          <div className="bg-gray-900 h-[300px]  p-4 flex justify-center items-center rounded-2xl relative">
-            <div className="absolute text-lg font-bold text-white top-4 right-6">
-              <button onClick={toggleVisible}>X</button>
-            </div>
-            <div className="flex flex-col text-gray-900 ">
-              <Popover open={open} onOpenChange={setOpen}>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded={open}
-                    className="w-[200px] justify-between"
-                  >
-                    {value}
+    <main className="absolute top-0 bottom-0 left-0 right-0 bg-black/40 ">
+      <div className="flex items-center justify-center h-[100vh] ">
+        <div className="bg-gray-900 h-[300px]  p-4 flex justify-center items-center rounded-2xl relative">
+          <div className="absolute text-lg font-bold text-white top-4 right-6">
+            <button onClick={toggleVisible}>X</button>
+          </div>
+          <div className="flex flex-col text-gray-900 ">
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="w-[200px] justify-between"
+                >
+                  {value}
 
-                    <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-[200px] h-[210px] p-0">
-                  <Command>
-                    <CommandInput placeholder="Search for Coins..." />
-                    <CommandEmpty>No Coin found.</CommandEmpty>
-                    <CommandGroup>
-                      {frameworks.map((framework) => (
-                        <CommandItem
-                          key={framework.value}
-                          onSelect={(currentValue) => {
-                            setValue(
-                              currentValue === value ? "" : currentValue
-                            );
-                            setOpen(false);
-                          }}
-                        >
-                          <Check
-                            className={cn(
-                              "mr-2 h-4 w-4",
-                              value === framework.value
-                                ? "opacity-100"
-                                : "opacity-0"
-                            )}
-                          />
-                          {framework.label}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-              <button
-                className="px-5 py-1 mt-4 text-gray-900 bg-white rounded-md"
-                onClick={() => submitData()}
-              >
-                Select
-              </button>
-            </div>
+                  <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-[200px] h-[210px] p-0">
+                <Command>
+                  <CommandInput placeholder="Search for Coins..." />
+                  <CommandEmpty>No Coin found.</CommandEmpty>
+                  <CommandGroup>
+                    {frameworks.map((framework) => (
+                      <CommandItem
+                        key={framework.value}
+                        onSelect={(currentValue) => {
+                          setValue(currentValue === value ? "" : currentValue);
+                          setOpen(false);
+                        }}
+                      >
+                        <Check
+                          className={cn(
+                            "mr-2 h-4 w-4",
+                            value === framework.value
+                              ? "opacity-100"
+                              : "opacity-0"
+                          )}
+                        />
+                        {framework.label}
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </Command>
+              </PopoverContent>
+            </Popover>
+            <button
+              className="px-5 py-1 mt-4 text-gray-900 bg-white rounded-md"
+              onClick={() => submitData()}
+            >
+              Select
+            </button>
           </div>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
 
