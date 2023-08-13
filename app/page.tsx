436370ad9de27d9530a786811ac94008b36263a5
Sabir222 <mrsabir4@gmail.com>
2023-08-13 19:36:30 +0100

    love  you vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import RegisterModal from "@/components/RegisterModal";
-import SigninModal from "@/components/SigninModal";
 import Table from "@/components/Table";
-import Test from "@/components/Test";
 
 export default function Home() {
   return (
